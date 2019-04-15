f2e972201c0d8568e9be304c8a3d6529cb974271
Kangyun Wang <kevinwang8011@hotmail.com>
2019-04-15 18:37:59 +0200

    Correction button for nodemcu

--- hardware/shelf/tests/buttonDebounce/buttonDebounce.ino
+++ hardware/shelf/tests/buttonDebounce/buttonDebounce.ino
@@ -1,10 +1,10 @@
 // const int buttonPin = 4;  // D1
 //// NodeMCU
-// const int buttonPin = 5;  // D2 & PWD
-// const int ledPin = LED_BUILTIN;
+const int buttonPin = 5;  // D2 & PWD
+const int ledPin = LED_BUILTIN;
 //// Uno
-const int buttonPin = 4;
-const int ledPin = 12;
+// const int buttonPin = 4;
+// const int ledPin = 12;
 
 int buttonState;      // the current reading from the input pin
 int lastButtonState;  // the previous reading from the input pin
@@ -35,6 +35,11 @@ void loop() {
     if ((millis() - lastDebounceTime) > debounceDelay) {
         // whatever the reading is at, it's been there for longer than the debounce
         // delay, so take it as the actual current state:
+
+        Serial.print(currentButtonState);
+        Serial.print(" - ");
+        Serial.print(buttonState);
+        Serial.println();
 
         // if the button state has changed:
         if (currentButtonState != buttonState) {
